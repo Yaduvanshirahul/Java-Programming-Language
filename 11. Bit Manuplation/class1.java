0217d49d61a5391f0940148454037cf6a813f5bd
Yaduvanshirahul <ry421072@gmail.com>
2023-09-07 00:00:29 +0530

    BIT Manuplation

---
+++ 11. Bit Manuplation/class1.java
@@ -0,0 +1,18 @@
+//  BIT MANIPULATION
+
+public class class1 {
+    public static void main(String[] args) {
+
+        System.out.println(5 & 7); // BINARY AND
+
+        System.out.println(5 | 7); // BINARY OR
+
+        System.out.println(5 ^ 7); // BINARY XOR
+
+        System.out.println(~6);// BINARY ONE'S COMPLIMENT
+
+        System.out.println(5 << 3);// BINARY LEFT SHIFT
+
+        System.out.println(5 >> 2);// BINARY RIGHT SHIFT
+    }
+}
