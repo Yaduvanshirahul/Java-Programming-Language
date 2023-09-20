d575f3d16b81194555c0af15248249aaaed2c905
Yaduvanshirahul <ry421072@gmail.com>
2023-09-20 22:57:59 +0530

    UPDATE

--- 12. OOPS/Class9.java
+++ 12. OOPS/Class9.java
@@ -1,35 +1,33 @@
-//  DEEP COPY  CONSTRUCTOR  
+//  SHALLOW   COPY  CONSTRUCTOR  
 
 public class Class9 {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
-        Marks m1 = new Marks();
-        m1.marks[0] = 11;
-        m1.marks[1] = 13;
-        m1.marks[2] = 12;
+        Marks m = new Marks();
+        m.marks[0] = 12;
+        m.marks[1] = 13;
+        m.marks[2] = 14;
 
-        Marks m2 = new Marks(m1);
-
-        m1.marks[2] = 14;
+        Marks m2 = new Marks(m);
+        m.marks[1] = 25;
         for (int i = 0; i < 3; i++) {
             System.out.println(m2.marks[i]);
         }
+
     }
 }
 
 class Marks {
     int marks[];
 
-    Marks(Marks m1) {
-        marks = new int[3]; // Initialize the marks array here
-
-        for (int i = 0; i < marks.length; i++) {
-            this.marks[i] = m1.marks[i];
-        }
+    Marks(Marks m) {
+        marks = new int[3];
+        this.marks = m.marks;
     }
 
     Marks() {
+        marks = new int[3];
         System.out.println("constructor is called");
     }
 }
